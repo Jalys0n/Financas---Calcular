ce36e3f947cb90576fb0efad572e60909378018c
Jose <jose.fontenele@intelit.com.br>
2023-02-28 12:04:30 -0300

    Função calcular a reserva de emergência sendo feita

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 function calculoreserva(){
 var salario, despesas,mes;
-    salario = document.getElementById("inputrenda").value;
-    despesas = document.getElementById('inputdividas').value;
-    mes = document.getElementById('inputmes').value;
-    
+            salario = document.getElementById("inputrenda").value;
+            despesas = document.getElementById('inputdividas').value;
+            mes = document.getElementById('inputmes').value;
+    total = despesas * mes;
+    guardarquantia = total/12; 
+
+        document.write(`A ideia de se manter ${mes} meses parados requer uma quantia de ${guardarquantia} por 12 meses, no mínimo.`)
 }
