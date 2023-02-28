45e76801402841cd6976b3ad87b243d08a0ac1f8
Jose <jose.fontenele@intelit.com.br>
2023-02-28 11:43:54 -0300

    Função sendo feita

---
+++ script.js
@@ -0,0 +1,9 @@
+/*ids: inputsalario ; inputrenda ; */
+
+function calculoreserva(){
+var salario, despesas,mes;
+    salario = document.getElementById("inputrenda").value;
+    despesas = document.getElementById('inputdividas').value;
+    mes = document.getElementById('inputmes').value;
+    
+}
